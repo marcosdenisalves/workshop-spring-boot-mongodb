fe7552f54d57fb97ebb437f700e0a1d624ef2ea2
Marcos Dênis <dee.alves2@gmail.com>
2020-06-20 20:58:01 -0300

    repository and service created with connection to mongodb

--- src/main/java/com/denisalves/workshopmongo/domain/User.java
+++ src/main/java/com/denisalves/workshopmongo/domain/User.java
@@ -4,9 +4,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
