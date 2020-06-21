c6e58be352718671595a0250c558b9b6b4e0e8e7
Marcos Dênis <dee.alves2@gmail.com>
2020-06-21 11:28:44 -0300

    author data projection with DTO pattern

--- src/main/java/com/denisalves/workshopmongo/domain/Post.java
+++ src/main/java/com/denisalves/workshopmongo/domain/Post.java
@@ -8,6 +8,8 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.denisalves.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -18,13 +20,13 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	
-	private User author;
+	private AuthorDTO author;
 	private List<Comment> comments = new ArrayList<>();
 	
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body,AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -65,11 +67,11 @@ public class Post implements Serializable{
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 	
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
