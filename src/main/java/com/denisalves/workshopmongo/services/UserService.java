2e9c534e0812b2b8c885e0b4b273fd184fec662e
Marcos Dênis <dee.alves2@gmail.com>
2020-06-21 00:29:09 -0300

    insert user

--- src/main/java/com/denisalves/workshopmongo/services/UserService.java
+++ src/main/java/com/denisalves/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.denisalves.workshopmongo.domain.User;
+import com.denisalves.workshopmongo.dto.UserDTO;
 import com.denisalves.workshopmongo.repository.UserRepository;
 import com.denisalves.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }
